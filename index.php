451b7245d88a9e8ddfca5b1ad3d5953b516ef2fd
Vijaya Rohith K <vijayarohith@gmail.com>
2021-12-10 19:16:22 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <html> 
 	<head> 
-	<title>PHP</title>
+	<title>PHP 2021  Rohith </title>
 	<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1"/>
 	</head>
 
 <body> 
-	<h1>PHP 2021  Rohith  Test5 </b></p>
+	<h1>PHP 2021  Rohith  Test1 </b></p>
 		<?php echo "The Current Date and yes las  Time is: <br />";
 		echo date("g:i A l, F j Y.");?> </p>
 	<h2>PHP Information VERSION1 </h2> 
